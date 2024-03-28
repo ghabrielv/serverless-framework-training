def22fdc0715eddd32bee4e99400446ba9954c01
Ghabriel Villarreal <ghabrielv@gmail.com>
2024-03-28 11:33:30 -0400

    Get user function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### Serverless framework
+# Serverless framework
+### Links
+Get users endpoint url: https://<API_ID>.execute-api.localhost.localstack.cloud:4566/dev/users/{id}
---
+++ getusers/handler.js
@@ -0,0 +1,37 @@
+const aws = require("aws-sdk")
+
+let dynamoDBClientParams = {}
+
+if (process.env.IS_OFFLINE) {
+    dynamoDBClientParams =  {
+        region: 'localhost',
+        endpoint: 'http://localhost:8000',
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+}
+
+const dynamodb = new aws.DynamoDB.DocumentClient(dynamoDBClientParams)
+
+const getUsers = async (event, context) => {
+
+    let userId = event.pathParameters.id
+
+    var params = {
+        ExpressionAttributeValues: { ':pk': userId },
+        KeyConditionExpression: 'pk = :pk',
+        TableName: 'usersTable'
+    };
+
+    return dynamodb.query(params).promise().then(res => {
+        console.log(res)
+        return {
+            "statusCode": 200,
+            "body": JSON.stringify({ 'user': res})
+        }
+    })
+}
+
+module.exports = {
+    getUsers
+}

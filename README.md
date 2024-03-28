d2edcfbafc42aabc59e2b7e16b07a72d111c0460
Ghabriel Villarreal <ghabrielv@gmail.com>
2024-03-28 14:30:19 -0400

    Update user function

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ### Links
 Get users endpoint GET: https://<API_ID>.execute-api.localhost.localstack.cloud:4566/dev/users/{id}
 Create users endpoint POST: https://<API_ID>.execute-api.localhost.localstack.cloud:4566/dev/users
+Update users endpoint PATCH: https://<API_ID>.execute-api.localhost.localstack.cloud:4566/dev/users/{id}
---
+++ updateUsers/handler.js
@@ -0,0 +1,43 @@
+const aws = require("aws-sdk")
+
+let dynamoDBClientParams = {}
+
+if (process.env.IS_OFFLINE) {
+    dynamoDBClientParams =  {
+        region: 'localhost',
+        endpoint: 'http://localhost:8000',
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+}
+
+const dynamodb = new aws.DynamoDB.DocumentClient(dynamoDBClientParams)
+
+const updateUsers = async (event, context) => {
+
+    let userId = event.pathParameters.id
+
+    const body = JSON.parse(event.body)
+
+    var params = {
+        TableName: 'usersTable',
+        Key: { pk: userId },
+        UpdateExpression: 'set #age = :age, #address = :address',
+        ExpressionAttributeNames: { '#age' : 'age', '#address' : 'address' },
+        ExpressionAttributeValues:
+            { ':age' : body.age, ':address' : body.address },
+        ReturnValues: 'ALL_NEW'
+    };
+
+    return dynamodb.update(params).promise().then(res => {
+        console.log(res)
+        return {
+            "statusCode": 200,
+            "body": JSON.stringify({ 'user': res.Attributes })
+        }
+    })
+}
+
+module.exports = {
+    updateUsers
+}
